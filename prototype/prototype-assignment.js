4c0a1de2ffce2a4a8638eb56258a8ef5bd821cfa
ongvang00 <you@example.com>
2023-04-11 15:56:30 -0400

    adas

---
+++ prototype/prototype-assignment.js
@@ -0,0 +1,42 @@
+
+
+function Person(firstName, lastName, favoriteColor, favoriteNumber, favoriteFood, family){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.favoriteColor = favoriteColor;
+    this.favoriteNumber = favoriteNumber;
+    this.favoriteFood = favoriteFood;
+
+}
+// Add a function on the Person.prototype called fullName that returns the firstName and lastName property of an object created by the Person constructor concatenated together.
+Person.prototype.fullname = function () {
+    console.log("Full Name: " + this.firstName + " " + this.lastName );
+}; 
+
+
+//Overwrite the toString method from the Object prototype by creating a toString method for Person. The toString method should return a string in the following format:
+Person.prototype.toString = function () {
+    console.log(this.firstName,  this.lastName + ', Favorite Color: ' + this.favoriteColor, ', Favorite Number: ' + this.favoriteNumber )
+}
+
+
+//Add a property on the Person object called family which is an empty array.
+Person.prototype.family = [];
+
+
+// Add a function on the Person.prototype called addToFamily which adds an object constructed from the Person constructor to the family 
+//array. To make sure that the object you are adding is an object constructed from the Person constructor - 
+//take a look at the instanceofoperator. Make sure that your family array does not include duplicates! 
+//This method should return the length of the family array.
+Person.prototype.addToFamily = function (familyMembers) { 
+this.family.push(familyMembers)
+console.log (this.family)
+console.log (this.family.length)
+return this.family.length
+
+}
+
+const Molly = new Person ('Molly', 'Smith', 'Green', '7', ['noodles', 'chicken'])
+const sister = new Person ('Sarah', 'Smith', 'blue', '9', 'pizza')
+
+Molly.addToFamily(sister)
