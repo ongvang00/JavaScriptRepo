19b169b2063490fc2cb78a8971a0765f92bcd6d5
ongvang00 <you@example.com>
2023-04-20 09:25:57 -0400

    asfa

--- promises/script.js
+++ promises/script.js
@@ -17,25 +17,34 @@ const users =
 }]
 
 //Implement a function called fetchUserById that accepts a user ID as an argument and returns a promise
-let fetchUserById = new Promise ((resolve,reject) => {
-    setTimeout(() => {
-//The promise should resolve with the user object if the user with the given ID exists in the users array. 
-        let id = " "
-            if (id === users.id){
-                resolve("Match")
-//Otherwise, it should reject with an error message. 
-            } else {
-                reject("Not Found")
-            }
-//Simulate an asynchronous request by using setTimeout() to delay the resolution or rejection of the promise by 1 second.
-    }, 1000)
-})
+// //The promise should resolve with the user object if the user with the given ID exists in the users array. 
+// //Otherwise, it should reject with an error message. 
+// //Simulate an asynchronous request by using setTimeout() to delay the resolution or rejection of the promise by 1 second.
 
-//Use the fetchUserById function to fetch the user with ID 1. Log the user object if the promise is fulfilled and the error message if the promise is rejected.
-fetchUserById.then((message)=>{
+function fetchUserById(userId) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (userId == users.id) {
+          resolve("ID Match");
+        } else {
+          reject("Error- ID not found");
+        }
+      }, 1000);
+    });
+  }
+
+  console.log("Starting fetch");
+
+fetchUserById(1)
+.then((message) => {
     console.log ("resolved:", message);
-});
-.catch ((message) => {
+})
+.catch((message) => {
     console.log("reject:", message);
 });
 
+console.log("Fetch Initiated");
+//Use the fetchUserById function to fetch the user with ID 1. Log the user object if the promise is fulfilled and the error message if the promise is rejected.
+
+
+//Next, chain another fetchUserById call to fetch the user with ID 2. Log the user object if the promise is fulfilled and the error message if the promise is rejected.
